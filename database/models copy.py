3ffac6ab90e0d6c3a3ca5601b8f00e902ead4bed
atatarikov <atatarikov@gmail.com>
2022-11-01 19:16:47 +0300

    init 2 commit

---
+++ database/models copy.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(60), nullable=False)
+    author = Column(String(30), nullable=False)
+    reviews = relationship('Reviews', backref='book', lazy=True)
+
+    def __repr__(self):
+        return self.title
+
+class Reviews(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(2000), nullable=False)
+    book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f'От {self.reviewer}'
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    reviews = relationship('Reviews', backref='reviewer', lazy=True)
+
+    def __repr__(self):
+        return self.name
